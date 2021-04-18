1cb57887613cc3282a48f1083a488b181cd45ff8
mario jose goes tarosso <mariojgt2@gmail.com>
2021-04-18 18:42:02 +0100

    boot token bug fix

--- Publish/Config/skeleton.php
+++ Publish/Config/skeleton.php
@@ -4,5 +4,5 @@ return [
     'version'         => '1.0.3',   // site version
     'register_enable' => false,     // if true users can register
     // Needed for the laravel sanctum
-    'token'           => 'skeleton_default_token',
+    'boot_token'           => 'skeleton_default_token',
 ];
