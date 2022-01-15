9f464800d8253ce4c7de93eed3b0e5de544c45fc
mario tarosso <mariojgt2@gmail.com>
2022-01-15 18:00:16 +0000

    sync

--- Publish/Resource/inertiajs/js/Pages/Dashboard/MyProfile.vue
+++ Publish/Resource/inertiajs/js/Pages/Dashboard/MyProfile.vue
@@ -1,30 +1,37 @@
 <template>
     <Layout>
         <div class="grid grid-cols-1 gap-6 lg:p-10 xl:grid-cols-12 lg:bg-base-200 rounded-box">
-
-                <div v-if="props.autenticator_enable == false"
-                    class="card col-span-1 row-span-3 shadow-lg xl:col-span-2 bg-base-100">
-                    <div class="card-body">
-                        <h2 class="my-4 text-4xl font-bold card-title">Enable 2 Steps Verification</h2>
-                        <div class="card row-span-3 shadow-lg compact bg-base-100 p-3">
-                            <figure v-html="props.autenticator.qr_code" ></figure>
-                            <div class="flex-row items-center space-x-4 card-body">
-                                <div>
-                                    <h2 class="card-title">You Autenticaton QRCode</h2>
-                                    <p class="text-base-content text-opacity-40">If you can't use QRCode you can use the screct <strong>{{ props.autenticator.secret }}</strong>
-                                    </p>
-                                    <strong>This Code will show only one time.</strong>
-                                </div>
+            <div v-if="props.autenticator_enable == false"
+                class="card col-span-1 row-span-3 shadow-lg xl:col-span-2 bg-base-100">
+                <div class="card-body">
+                    <h2 class="my-4 text-4xl font-bold card-title">Enable 2 Steps Verification</h2>
+                    <div class="card row-span-3 shadow-lg compact bg-base-100 p-3">
+                        <figure v-html="props.autenticator.qr_code" ></figure>
+                        <div class="flex-row items-center space-x-4 card-body">
+                            <div>
+                                <h2 class="card-title">You Autenticaton QRCode</h2>
+                                <p class="text-base-content text-opacity-40">If you can't use QRCode you can use the screct <strong>{{ props.autenticator.secret }}</strong>
+                                </p>
+                                <strong>This Code will show only one time.</strong>
                             </div>
                         </div>
-                        <!-- <x-skeleton::form.form action="{{ route('skeleton.2fa.enable') }}">
-                            <div class="px-5 py-7">
-                                <x-skeleton::form.text name="code" label="Type the code here, if successfully 2 steps will be enabled" />
-                                <x-skeleton::form.submit name="Check Code" />
-                            </div>
-                        </x-skeleton::form.form> -->
                     </div>
+                    <x-skeleton::form.form action="{{ route('skeleton.2fa.enable') }}">
+                        <div class="px-5 py-7">
+                            <input-field
+                                v-model="email"
+                                label="Code"
+                                type="code"
+                                name="code"
+                                id="code"
+                                placeholder="Type the code here"
+                            />
+
+                            <submit name="submit" @click="code" />
+                        </div>
+                    </x-skeleton::form.form>
                 </div>
+            </div>
         </div>
 
     </Layout>
@@ -42,8 +49,7 @@ import InputPassword from "../../Components/Form/Password";
 import Submit from "../../Components/Form/Submit";
 import LinkButton from "../../Components/Form/Link.vue";
 
-let email = $ref("");
-let password = $ref("");
+let code = $ref("");
 
 const props = defineProps({
     autenticator: {
@@ -58,8 +64,7 @@ const props = defineProps({
 
 const submitForm = () => {
     const form = {
-        email: email,
-        password: password,
+        code: code,
     };
     Inertia.post("/login/user", form);
 };
